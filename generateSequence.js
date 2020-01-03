931abe6a8ff40576c21c21b171cffa45281f62be
Filipe Werneck <filipeglfw@gmail.com>
2020-01-03 10:03:14 -0300

    Improved random generator

--- generateSequence.js
+++ generateSequence.js
@@ -10,11 +10,38 @@ function getStrongRandomArbitrary(min, max) {
     return getStrongRandom() * (max - min) + min;
 };
 
-function megaSena() {
+function getStrongRandomInteger(min, max) {
+  const range = max - min;
+  const maxGeneratedValue = Math.pow(2,32)-1;
+  const possibleResultValues = range + 1;
+  const possibleGeneratedValues = maxGeneratedValue + 1;
+  const remainder = possibleGeneratedValues % possibleResultValues;
+  const maxUnbiased = maxGeneratedValue - remainder;
+
+  if (!Number.isInteger(min) || !Number.isInteger(max) ||
+       max > Number.MAX_SAFE_INTEGER || min < Number.MIN_SAFE_INTEGER) {
+    throw new Error('Arguments must be safe integers.');
+  } else if (range > maxGeneratedValue) {
+    throw new Error(`Range of ${range} (from ${min} to ${max}) > ${maxGeneratedValue}.`);
+  } else if (max < min) {
+    throw new Error(`max (${max}) must be >= min (${min}).`);
+  } else if (min === max) {
+    return min;
+  } 
+
+  let generated;
+  do {
+    generated = getStrongRandom()*maxGeneratedValue;
+  } while (generated > maxUnbiased);
+
+  return min + (generated % possibleResultValues);
+};
+
+function getNewSequence(len) {
     let sequence = [];
     let number;
-    while (sequence.length < 6) {
-        number = Math.floor(getStrongRandomArbitrary(1, 60));
+    while (sequence.length < len) {
+        number = Math.floor(getStrongRandomInteger(1, 60));
         if (!sequence.includes(number)) {
             sequence.push(number)
         }
